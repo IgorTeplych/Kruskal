2be50caae8be1065853a35612b635643d40b34b1
Igor Teplych <igorvrt@mail.ru>
2023-01-17 20:31:53 +0600

    Добавьте файлы проекта.

---
+++ Kruskal/Graf.cs
@@ -0,0 +1,55 @@
+﻿using BasicStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kruskal
+{
+    internal class Graf
+    {
+        int[] Parrent;
+
+        public Edge[] Kruskal(int[][] g)
+        {
+            Parrent = new int[g.Length];
+            for (int i = 0; i < Parrent.Length; i++) Parrent[i] = i;
+
+            SingleArray<Edge> edges = new SingleArray<Edge>();
+            while (edges.Size < g.Length - 1)
+            {
+                int v1 = 0, v2 = 0;
+                int minWeight = int.MaxValue;
+                for (int i = 0; i < g.Length; i++)
+                    for (int j = 0; j < g[i].Length; j++)
+                        if (g[i][j] != 0 && g[i][j] < minWeight)
+                        {
+                            v1 = i;
+                            v2 = j;
+                            minWeight = g[i][j];
+                        }
+
+                if (Find(v1) != Find(v2))
+                {
+                    edges.Add(new Edge(v1, v2), edges.Size);
+                    Union(v1, v2);
+                }
+                g[v1][v2] = int.MaxValue;
+                g[v2][v1] = int.MaxValue;
+            }
+            return edges.Array;
+        }
+        int Find(int a)
+        {
+            if (Parrent[a] == a) return a;
+            return Find(Parrent[a]);
+        }
+        void Union(int x, int y)
+        {
+            int parX = Find(x);
+            int parY = Find(y);
+            Parrent[parX] = parY;
+        }
+    }
+}
---
+++ Kruskal/Program.cs
@@ -0,0 +1,27 @@
+﻿using Kruskal;
+
+class Programm
+{
+    public static void Main()
+    {
+        int[][] vect = new int[6][];
+        vect[0] = new int[] { 1, 2 };
+        vect[1] = new int[] { 0, 2, 3 };
+        vect[2] = new int[] { 0, 1, 3, 4 };
+        vect[3] = new int[] { 1, 2, 4, 5 };
+        vect[4] = new int[] { 2, 3, 5 };
+        vect[5] = new int[] { 3, 4 };
+
+        int m = int.MaxValue;
+        int[][] vect2 = new int[6][];
+        vect2[0] = new int[6] { 0, 7, 8, m, m, m };
+        vect2[1] = new int[6] { 7, 0, 11, 2, m, m };
+        vect2[2] = new int[6] { 8, 11, 0, 6, 9, m };
+        vect2[3] = new int[6] { m, 2, 6, 0, 11, 9 };
+        vect2[4] = new int[6] { m, m, 9, 11, 0, 10 };
+        vect2[5] = new int[6] { m, m, m, 9, 10, 0 };
+
+        Graf graf = new Graf();
+        var vr = graf.Kruskal(vect2);
+    }
+}
